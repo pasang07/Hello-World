dafdbeb3ad8789a19ce931706ef8e34c10d55bba
Pasang Yangji Tamang <pasang@Pasangs-MacBook-Pro.local>
2023-08-09 22:09:57 +1000

    updated

---
+++ src/main/java/com/mycompany/assignment1v1/Assignment1V1.java
@@ -0,0 +1,10 @@
+package com.mycompany.assignment1v1;
+/**
+ *
+ * @author 12210663
+ */
+public class Assignment1V1 {
+    public static void main(String[] args) {
+        System.out.println(" Hello World ");
+    }
+}
